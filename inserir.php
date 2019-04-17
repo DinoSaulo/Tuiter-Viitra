6fc3e67c1a8a1a796e534c01b95b9253ff15aa23
Mariana Lima <mlsb@cin.ufpe.br>
2019-04-17 17:09:28 -0300

    first commit

---
+++ inserir.php
@@ -0,0 +1,10 @@
+<?php
+
+    $link = mysqli_connect("localhost", "root", "vertrigo", "csgodouble");
+
+    $contador = "0";
+    while($contador < 100){
+        $contador++;
+        $sql = mysqli_query($link, "");
+    }
+?>
